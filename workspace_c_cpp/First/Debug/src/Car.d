10ed38ab76f364e12bcb453a3bc8eadf5df250c3
xutu <billxu89@gmail.com>
2017-04-28 17:36:21 +0200

    delete workspace

--- workspace_c_cpp/First/Debug/src/Car.d
+++
@@ -1,7 +0,0 @@
-src/Car.o: ../src/Car.cpp ../src/Car.h ../src/Vehicle.h ../src/Wheel.h
-
-../src/Car.h:
-
-../src/Vehicle.h:
-
-../src/Wheel.h:
--- workspace_c_cpp/First/Debug/src/Wheel.d
+++
@@ -1,3 +0,0 @@
-src/Wheel.o: ../src/Wheel.cpp ../src/Wheel.h
-
-../src/Wheel.h:
