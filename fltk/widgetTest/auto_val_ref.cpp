6171024eb695c319b62363cd410b3be261ba63c4
xutu <billxu89@gmail.com>
2020-09-29 21:52:13 +0200

    set.find, std::find()

--- fltk/widgetTest/auto_val_ref.cpp
+++ fltk/widgetTest/auto_val_ref.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <set>
+#include <algorithm>
 #include<stdio.h>
 using namespace std;
 /*
-  auto it : s iterates by value and ref
+1.  auto it : s iterates by value and ref
   
   in this there is no difference by value and ref
- */
+
+2. set.find(Point), set.erase(Point) not work. set.erase(it) ok
+   instead std::find() can be used
+
+*/
 
 
 struct Point{
@@ -27,24 +32,39 @@ struct Point{
 	// *it as this is passed to this function, in this case function must be const
 	bool operator==(const Point& p) const{ 
 		return (p.x == x) && (p.y == y);
-}
+	}
 
 };
 
 int main(){
 
 	std::set<Point> s;
-	Point p(1,1);
-	Point p2(2,2);
+	// Point p(1,1);
+	// Point p2(2,2);
 	
-	s.insert(p);
-	s.insert(p2);
+	s.insert(Point(1,1));
+	s.insert(Point(2,2));
 	// not work???
 	//	s.erase(p);
-	
-	printf("address of p : %p \n", p);  // 0x01
+
+	for(auto it : s){
+		cout<<it.x<<","<<it.y<<endl;
+	}
 
 	Point tmp(1,1);
+
+	// ok
+	// auto if_find = std::find(s.begin(), s.end(), tmp) ;
+	// if(if_find != s.end())
+	// 	s.erase(if_find);
+
+	// ok
+	s.erase(std::find(s.begin(), s.end(), tmp));
+	
+	for(auto it : s){
+		cout<<it.x<<","<<it.y<<endl;
+	}
+	
 	
 	// not work
 	// for(auto &it : s){
@@ -62,11 +82,12 @@ int main(){
 	// 		s.erase(it);
 	// }
 
-	
-	for(auto it : s){
-		cout<<it.x<<","<<it.y<<endl;
-		printf("value of it :%p \n",it);
-	}
+	// auto it = s.find(tmp);
+	// if(it != s.end()){
+	// 	s.erase(it);
+	// 	cout<<"deleted"<<endl;
+	// }
+		
 	
 	return 0;	
 }
